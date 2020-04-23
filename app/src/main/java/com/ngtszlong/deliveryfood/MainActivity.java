cd8de0fd701c5a9541b11f91171e53948b07d932
Long <ngtszlong2@yahoo.com.hk>
2020-04-23 16:22:25 +0800

    Commit

--- app/src/main/java/com/ngtszlong/deliveryfood/MainActivity.java
+++ app/src/main/java/com/ngtszlong/deliveryfood/MainActivity.java
@@ -1,14 +1,110 @@
 package com.ngtszlong.deliveryfood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    Toolbar toolbar;
+    SearchView searchView;
+    TextView find_location;
+    LocationManager locationManager;
+    int REQUEST_LOCATION = 123;
+    double latitude;
+    double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+        }
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        searchView = findViewById(R.id.searchview);
+        searchView.onActionViewExpanded();
+        searchView.setIconifiedByDefault(true);
+
+        find_location = findViewById(R.id.location);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+        onLocationChanged(location);
+        getlocationname(location);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.person_detail:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    private void getlocationname(Location location) {
+        try {
+            Geocoder geocoder = new Geocoder(this);
+            List<Address> addressList = null;
+            addressList = geocoder.getFromLocation(latitude, longitude, 1);
+            String address = addressList.get(0).getFeatureName();
+            find_location.setText(address);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
